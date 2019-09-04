35f28f063d368599cf273aac2dc0fdb3aa1b0b30
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-04 18:13:14 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,10 +20,10 @@ def bonus
    }
   }
 
-  bonus[:montague][:hero][:status] = "dead",
-  bonus[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   
-  
+  puts epic_tragedy
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> 12480eb3065008fab37859c2ef7f79261c0376b7
       :patriarch => {},
       :matriarch => {},
       :hero => {},
