0d85af0c08f8cf88739101a35e951583541a0689
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-04 18:26:06 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,6 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 12480eb3065008fab37859c2ef7f79261c0376b7
       :patriarch => {},
       :matriarch => {},
       :hero => {},
